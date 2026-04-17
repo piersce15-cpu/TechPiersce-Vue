9bdc7506a7a366649dfc1ddc57622869f62ad90e
piersce15-cpu <piersce15@gmail.com>
2026-04-17 10:58:19 -0400

    Configuración final de despliegue sin guiones bajos

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,26 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/TechPiersce-Vue/',
+  build: {
+    rollupOptions: {
+      output: {
+        // Esto cambia la carpeta de 'assets' a 'static' para los archivos JS
+        // Así evitamos el bloqueo de GitHub sin romper el build
+        chunkFileNames: 'assets/js-[name]-[hash].js',
+        entryFileNames: 'assets/js-[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
     },
   },
   test: {
     environment: 'jsdom',
     globals: true,
   },
-  base: '/TechPiersce-Vue/',
 })
